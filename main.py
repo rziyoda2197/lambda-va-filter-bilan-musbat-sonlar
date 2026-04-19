2f33f2e40bf2a714c01abf74cb2e1165561593df
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-19 19:48:24 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,13 @@
+def musbat_sonlar(ro'yxat):
+    return list(filter(lambda x: x > 0, ro'yxat))
+
+ro'yxat = [-1, 2, -3, 4, -5, 6]
+print(musbat_sonlar(ro'yxat))
+```
+
+```python
+def musbat_sonlar(ro'yxat):
+    return list(filter(lambda x: x > 0, ro'yxat))
+
+ro'yxat = [-1, 2, -3, 4, -5, 6]
+print(musbat_sonlar(ro'yxat))  # [2, 4, 6]
